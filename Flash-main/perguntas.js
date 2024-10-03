11e4076f164b2219a77c91db6a941abf3e1736d3
wh-soares <159548446+wh-soares@users.noreply.github.com>
2024-10-03 12:24:07 -0300

    Add files via upload

---
+++ Flash-main/perguntas.js
@@ -0,0 +1,155 @@
+criaCartao(
+    'CIÊNCIAS',
+    'Qual é a formúla química da água?',
+    'H²O?'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'No que se refere a Primeira Lei de Newton?',
+    'Todo corpo em repouso ou em movimento tende a ficar em seu estado de movimento á não ser que sofra uma força externa'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'Qual o livro que Charles Darwin escreveu e mudou a forma como vemos a evolução ?',
+    'A Origem das Especíes'
+)
+criaCartao(
+    'CIÊCIAS',
+    'Qual reino pertence a raposa?',
+    'Reino Animallia'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'Do que são formadas as nebulosas?',
+    'Hidrogênio, poeira cósmisca ,plasma.'
+)
+criaCartao(
+    'CIÊNCIAS',
+    'Como é denomidada uma molécula orgânica que só apresenta Hidrogênio e Carbono?',
+    'Hidrocarboneto'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á qual das duas pertence o Homem-Areia?',
+    'Marvel'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á quem pertence o Homem-Aranha?',
+    'Marvel'
+)
+criaCartao(
+    'DC ou Marvel',
+    'Quem é o melhor vilão entre as duas?',
+    'Coringa (DC)'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á quem pertence o vilão denomidado Duas Caras?',
+    'DC'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Á quem pertence o anti-herói chamado de "Pacificador"?',
+    'DC'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'A frase "Ai eu eu descansaria num Universo Agradecido" foi dita por?',
+    'Vilão da Marvel, Thanos'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual é a fraqueza do Batman?',
+    'As pessoas que ele cria um laço'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual das franquias possui os melhores Super-Heróis?',
+    'DC, não se engane.'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual dos universos tem os melhores filmes?',
+    'Marvel'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual dos universos tem as melhores séries?',
+    'DC'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual a melhor pessoa pro Batman?',
+    'É a Batgirl.'
+)
+criaCartao(
+    'DC ou Marvel?',
+    'Qual o filme que mais acumulou dinheiro pra Marvel',
+    'Vingadores, Ultimato'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
+criaCartao(
+    'Edita?',
+    'edita?',
+    'edita?'
+)
